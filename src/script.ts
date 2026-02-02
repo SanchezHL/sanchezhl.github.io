32ea5e1859a16c94120790bee54847be010b654a
David Sanchez-Hermosilla <david.sanchez.hermo@gmail.com>
2026-02-02 23:39:33 +0100

    Migration to typescript

--- src/script.ts
+++ src/script.ts
@@ -2,11 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.getElementById("menuToggle");
   const navLinks = document.getElementById("navLinks");
 
-  menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("show");
+  menuToggle?.addEventListener("click", function () {
+    navLinks?.classList.toggle("show");
   });
 });
-
-const unused = "main script";
-
-const position = { x: 1, y: 3 };
